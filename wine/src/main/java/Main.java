2310245ee8ea62e818f2dfe466a8a799a92d378d
leobel99 <leobel99@gmail.com>
2021-12-09 11:28:15 +0100

    Aggiunta Main

---
+++ wine/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import databases.*;
+
+public class Main {
+    public static void main(String[] args){
+        //crud_mongo mongo = new crud_mongo();
+
+        /*mongo.createReview("85","Prova","Wine was good","Giovanni","@giovanni 98",
+                20,"pdor","kmer","Puglia","","Bari","Italy","Ressa");*/
+
+        //mongo.deleteReviewsByTaster_Name("Giovanni ");
+
+        //mongo.findReviewByWinery("Ressa ");
+
+        //mongo.updateAllPrice(20,23);
+    }
+}
