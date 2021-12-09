19dea71f67ac8ff6b7b606fa14dc6a523747231b
leobel99 <leobel99@gmail.com>
2021-12-09 14:17:47 +0100

    TopK User

--- wine/src/main/java/databases/advanced_mongo.java
+++ wine/src/main/java/databases/advanced_mongo.java
@@ -3,28 +3,48 @@ package databases;
 //here there are all the advanced query with aggregation
 //top k user with average highest of review
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.*;
+import com.mongodb.client.*;
 import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.BsonField;
+import com.mongodb.client.model.Projections;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import java.util.Arrays;
+
 import static com.mongodb.client.model.Filters.*;
 
 public class advanced_mongo {
 
-
-
+    //Top k user with the highest average of his review scores
+    public void topKuser () {
+        final MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        MongoDatabase database = mongoClient.getDatabase("wine");
+        MongoCollection<Document> collection = database.getCollection("review");
+        collection.aggregate(
+                Arrays.asList(
+                        Aggregates.project(
+                                Projections.fields(
+                                        Projections.excludeId(),
+                                        Projections.include("taster_name"),
+                                        Projections.computed(
+                                                "Highest average",
+                                                new Document("$max", Arrays.asList("$avg", new BsonString("$points")))
+                                        )
+                                )
+                        )
+                )
+        ).forEach((Block<? super Document>) doc -> System.out.println(doc.toJson()));
+    }
 }
