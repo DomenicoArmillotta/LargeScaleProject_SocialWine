29b35a5175d63dc731b47721988cb36128860da7
leobel99 <leobel99@gmail.com>
2021-12-16 14:57:19 +0100

    Login system (first shape)

---
+++ wine/src/main/java/miscellaneous/loginSystem.java
@@ -0,0 +1,32 @@
+package miscellaneous;
+
+import java.util.Scanner;
+
+public class loginSystem {
+
+    public void logIn(){
+        String Username = "admin";
+        String Password = "root";
+
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Enter Username : ");
+        String username = input1.next();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Enter Password : ");
+        String password = input2.next();
+
+        if (username.equals(Username) && password.equals(Password)) {
+            System.out.println("Access Granted! Welcome!");
+        }
+
+        else if (username.equals(Username)) {
+            System.out.println("Invalid Password!");
+        } else if (password.equals(Password)) {
+            System.out.println("Invalid Username!");
+        } else {
+            System.out.println("Invalid Username & Password!");
+        }
+    }
+
+}
