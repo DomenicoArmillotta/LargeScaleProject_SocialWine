17b69cef76b0ff5c7abf482fb9d40c1eb4947df5
leobel99 <leobel99@gmail.com>
2021-12-19 17:55:53 +0100

    Admin LogIn System

--- wine/src/main/java/miscellaneous/loginAdmin.java
+++ wine/src/main/java/miscellaneous/loginAdmin.java
@@ -1,13 +1,21 @@
 package miscellaneous;
 
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.DeleteResult;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
 import java.util.Scanner;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class loginAdmin {
 
-    public boolean logIn(){
-        String Username = "admin";
-        String Password = "root";
-
+    public boolean logIn() {
         Scanner input1 = new Scanner(System.in);
         System.out.println("Enter Username : ");
         String username = input1.next();
@@ -16,15 +24,13 @@ public class loginAdmin {
         System.out.println("Enter Password : ");
         String password = input2.next();
 
-        if (username.equals(Username) && password.equals(Password)) {
+        if (username.equals(getNameAdmin()) && password.equals(getPwdAdmin())) {
             System.out.println("Access Granted! Welcome!");
             return true;
-        }
-
-        else if (username.equals(Username)) {
+        } else if (username.equals(getNameAdmin())) {
             System.out.println("Invalid Password!");
             return false;
-        } else if (password.equals(Password)) {
+        } else if (password.equals(getPwdAdmin())) {
             System.out.println("Invalid Username!");
             return false;
         } else {
@@ -33,4 +39,50 @@ public class loginAdmin {
         }
     }
 
+    protected void addAdmin() {
+        final MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        MongoDatabase database = mongoClient.getDatabase("wine");
+        MongoCollection<Document> collection = database.getCollection("user_credentials");
+
+        Document document = new Document();
+        document.append("Name", "admin");
+        document.append("Password", "root");
+        
+        try {
+            collection.insertOne(document);
+            System.out.println("Successfully inserted documents. \n");
+        } catch (MongoWriteException mwe) {
+            if (mwe.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
+                System.out.println("Document with that id already exists");
+            }
+        }
+        mongoClient.close();
+    }
+
+    private String getNameAdmin() {
+        final MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        MongoDatabase database = mongoClient.getDatabase("wine");
+        MongoCollection<Document> collection = database.getCollection("user_credentials");
+        Bson filter = eq("Name", "admin");
+        FindIterable<Document> resultName = collection.find(filter);
+        String name = "";
+        for (Document document : resultName) {
+            name = name + document.toJson() + "\n";
+        }
+        return name;
+    }
+
+    private String getPwdAdmin() {
+        final MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        MongoDatabase database = mongoClient.getDatabase("wine");
+        MongoCollection<Document> collection = database.getCollection("user_credentials");
+        Bson filter = eq("Password", "root");
+        FindIterable<Document> resultPwd = collection.find(filter);
+        String pwd = "";
+        for (Document document : resultPwd) {
+            pwd = pwd + document.toJson() + "\n";
+        }
+        return pwd;
+    }
 }
+
