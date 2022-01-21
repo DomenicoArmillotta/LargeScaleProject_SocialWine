85d1993b6c164584964791ed713600b89dc322e2
Domenico <10582521@polimi.it>
2022-01-21 16:49:42 +0100

    modify suggested function

--- wine/src/main/java/databases/Advanced_graph.java
+++ wine/src/main/java/databases/Advanced_graph.java
@@ -111,6 +111,9 @@ public class Advanced_graph implements AutoCloseable {
 
 
 
+
+
+
     public ArrayList<Wine> showSuggestedWineByLike (final String username) {
         ArrayList<Wine> suggestedWines;
         try (Session session = driver.session()) {
@@ -125,7 +128,6 @@ public class Advanced_graph implements AutoCloseable {
                 while (result.hasNext()) {
                     Record r = result.next();
                     //User u = new User(r.get("username").asString() , "",r.get("twitter_taster_handle").asString() ,r.get("country").asString()  ,"", false);
-
                     Wine u = new Wine(r.get("wineName").asString(),r.get("designation").asString(),Integer.parseInt(r.get("price").asString()),r.get("province").asString(),r.get("variety").asString(),r.get("winery").asString(),null);
                     wines.add(u);
                 }
@@ -162,6 +164,76 @@ public class Advanced_graph implements AutoCloseable {
         }
         return suggestedWines;
     }
+
+
+
+
+    public ArrayList<User> showSuggestedUserByLikeAndFriends (final String username) {
+        ArrayList<User> suggestedUsers;
+        try (Session session = driver.session()) {
+            suggestedUsers = session.readTransaction((TransactionWork< ArrayList<User>>) tx -> {
+                Result result = tx.run("CALL{ \n"+
+                                "MATCH (n:User{username: $username})-[:Like]->(:Post)<-[:Created]-(u:User)\n" +
+                                "WHERE NOT EXISTS ((n)-[:Follow]->(u))\n" +
+                                "WITH u, rand() AS number\n" +
+                                "RETURN u ORDER BY number \n" +
+                                "UNION \n"+
+                                "MATCH (n:User{username: $username})-[:Follow]->(:User)<-[:Follow]-(u:User)\n" +
+                                "WHERE NOT EXISTS ((n)-[:Follow]->(u))\n" +
+                                "WITH u, rand() AS number\n" +
+                                "RETURN u ORDER BY number \n" +
+                                "}\n"+
+                                "RETURN u.username AS username , u.country as country , u.twitter_taster_handle as twitter_taster_handle LIMIT 10 ",
+                        parameters("username", username));
+                ArrayList<User> users = new ArrayList<>();
+                while (result.hasNext()) {
+                    Record r = result.next();
+                    User u = new User(r.get("username").asString() , "",r.get("twitter_taster_handle").asString() ,r.get("country").asString()  ,"", false);
+                    users.add(u);
+                }
+                return users;
+            });
+        } catch (Exception e){
+            suggestedUsers = null;
+        }
+        return suggestedUsers;
+    }
+
+
+
+    public ArrayList<Wine> showSuggestedWineByCommentAndLike (final String username) {
+        ArrayList<Wine> suggestedWines;
+        try (Session session = driver.session()) {
+            suggestedWines = session.readTransaction((TransactionWork< ArrayList<Wine>>) tx -> {
+                Result result = tx.run("CALL{\n"+
+                              "MATCH (n:User{username: $username})-[:Created]->(:Post)-[:Related]->(u:Wine)\n" +
+                                "WITH u, rand() AS number\n" +
+                                "RETURN u ORDER BY number\n" +
+                                "UNION\n" +
+                                "MATCH (n:User{username: $username})-[:Like]->(:Post)-[:Related]->(u:Wine)\n" +
+                                "WITH u, rand() AS number\n" +
+                                "RETURN u ORDER BY number\n" +
+                                "}\n" +
+                                "RETURN u.wineName AS wineName , u.price as price , u.designation as designation , u.winery as winery , u.variety as variety , u.province as province LIMIT 6 \n",
+                        parameters("username", username));
+                ArrayList<Wine> wines = new ArrayList<>();
+                while (result.hasNext()) {
+                    Record r = result.next();
+                    //User u = new User(r.get("username").asString() , "",r.get("twitter_taster_handle").asString() ,r.get("country").asString()  ,"", false);
+
+                    Wine u = new Wine(r.get("wineName").asString(),r.get("designation").asString(),Integer.parseInt(r.get("price").asString()),r.get("province").asString(),r.get("variety").asString(),r.get("winery").asString(),null);
+                    wines.add(u);
+                }
+                return wines;
+            });
+        } catch (Exception e){
+            suggestedWines = null;
+        }
+        return suggestedWines;
+    }
+
+
+
 
 
 
